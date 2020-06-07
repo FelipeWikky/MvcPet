2cf3f5e7279b155ba109935a42ffbb55ec024403
FelipeWikky <51749871+FelipeWikky@users.noreply.github.com>
2020-06-07 13:53:42 -0300

    Personalizando Identity pages

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -9,19 +9,20 @@ namespace MvcPet.Models
 
     [Display(Name = "Espécie")]
     public string species { get; set; } //espécie
+    
     [Display(Name = "Raça")]
     public string breed { get; set; } //raça
 
-    [Display(Name = "Descrição Pet")]
+    [Display(Name = "Descrição do Pet")]
     public string description { get; set; } //descrição
 
     public string image {get;set;}
 
-    [Display(Name = "Data de Entrada")]
+    [Display(Name = "Data do Cadastro")]
     [DataType(DataType.Date)]
     public DateTime created { get; set; }
 
-  [Display(Name = "Doador")]
-    public User donor { get; set; }
+  // [Display(Name = "Doador")]
+  //   public User donor { get; set; }
   }
 }
