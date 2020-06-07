3cc45df5b49c1c4c62823f4e8c134420d850ac6c
FelipeWikky <51749871+FelipeWikky@users.noreply.github.com>
2020-06-06 23:00:35 -0300

    Implementado Identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcPet.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(MvcPet.Areas.Identity.IdentityHostingStartup))]
+namespace MvcPet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MvcPetIdentityDbContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("MvcPetIdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MvcPetIdentityDbContext>();
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,7 @@ namespace MvcPet
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +52,7 @@ namespace MvcPet
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
