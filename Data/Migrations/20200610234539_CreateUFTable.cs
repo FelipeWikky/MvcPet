1293d5544e7bc492416426e7733bde6924e1e39d
FelipeWikky <51749871+FelipeWikky@users.noreply.github.com>
2020-06-10 20:46:58 -0300

    Nova Migration para criação da tabela UFs

---
+++ Data/Migrations/20200610234539_CreateUFTable.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityMvc.Data.Migrations
+{
+    public partial class CreateUFTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // migrationBuilder.DropForeignKey(
+            //     name: "FK_Pets_Animals_animalid",
+            //     table: "Pets");
+
+            // migrationBuilder.AlterColumn<int>(
+            //     name: "animalid",
+            //     table: "Pets",
+            //     nullable: false,
+            //     oldClrType: typeof(int),
+            //     oldType: "INTEGER",
+            //     oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Ufs",
+                columns: table => new
+                {
+                    sigla = table.Column<string>(nullable: false),
+                    nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ufs", x => x.sigla);
+                });
+
+            // migrationBuilder.AddForeignKey(
+            //     name: "FK_Pets_Animals_animalid",
+            //     table: "Pets",
+            //     column: "animalid",
+            //     principalTable: "Animals",
+            //     principalColumn: "id",
+            //     onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // migrationBuilder.DropForeignKey(
+            //     name: "FK_Pets_Animals_animalid",
+            //     table: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Ufs");
+
+            // migrationBuilder.AlterColumn<int>(
+            //     name: "animalid",
+            //     table: "Pets",
+            //     type: "INTEGER",
+            //     nullable: true,
+            //     oldClrType: typeof(int));
+
+            // migrationBuilder.AddForeignKey(
+            //     name: "FK_Pets_Animals_animalid",
+            //     table: "Pets",
+            //     column: "animalid",
+            //     principalTable: "Animals",
+            //     principalColumn: "id",
+            //     onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
