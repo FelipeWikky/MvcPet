00c502fc9e8810d314e76eb95c9fbee3df4c1ff7
FelipeWikky <51749871+FelipeWikky@users.noreply.github.com>
2020-06-13 14:11:20 -0300

    Page Error404 para rotas Not Found

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,52 +16,61 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace MvcPet.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly MvcPetContext _context;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public HomeController(
+        ILogger<HomeController> logger,
+        MvcPetContext context,
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly MvcPetContext _context;
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-
-        public HomeController(
-            ILogger<HomeController> logger, 
-            MvcPetContext context,
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
-        {
-            _logger = logger;
-            _context = context;
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            if ( _signInManager.IsSignedIn(User) ) {
-                var users = from m in _context.Users select m;
-                
-                // users = users.Where( u => u.userId.Equals( _userManager.GetUserId(User) ) );
-
-                // return View(await users.ToListAsync() );
-                var user = await _context.Users.FirstOrDefaultAsync(m => m.userId == _userManager.GetUserId(User) );
-                UserPetViewModel vm = new UserPetViewModel();
-                vm.User = user;
-                // return View(user);
-                return View(vm);
-            } else {
-                return View();
-            }
-        }
-
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
+      _context = context;
+      _signInManager = signInManager;
+      _userManager = userManager;
     }
+
+    public async Task<IActionResult> Index()
+    {
+      if (_signInManager.IsSignedIn(User))
+      {
+        var users = from m in _context.Users select m;
+
+        // users = users.Where( u => u.userId.Equals( _userManager.GetUserId(User) ) );
+
+        // return View(await users.ToListAsync() );
+        var user = await _context.Users.FirstOrDefaultAsync(m => m.userId == _userManager.GetUserId(User));
+        UserPetViewModel vm = new UserPetViewModel();
+        vm.User = user;
+        // return View(user);
+        return View(vm);
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    public IActionResult About()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult Error404()
+    {
+      return View();
+    }
+
+  }
 }
